694a0306217cdb8e528cc9a9f7284d674e3fc6fb
May-Hemade <may.hemade1993@gmail.com>
2022-01-11 16:34:43 +0200

    using hooks

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,91 +1,90 @@
-import { Component } from "react";
-import { Button, Form } from 'react-bootstrap'
+import { useState, useEffect } from "react"
 
-class AddComment extends Component {
+import { Button, Form } from "react-bootstrap"
 
-    state = {
-        comment: {
-            comment: '',
-            rate: 1,
-            elementId: null
+const AddComment = ({ asin }) => {
+  const [comment, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: null,
+  })
+
+  useEffect(() => {
+    setComment({
+      ...comment,
+      elementId: asin,
+    })
+  }, [asin])
+
+  const sendComment = async (e) => {
+    e.preventDefault()
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments",
+        {
+          method: "POST",
+          body: JSON.stringify(comment),
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWRjM2ZlM2YyNjM3ODAwMTVlNTBkMjMiLCJpYXQiOjE2NDE5MDg0NTMsImV4cCI6MTY0MzExODA1M30.EdzYJUXtXimORoGl8bgWRQ3JSeQPmTemWyvDbJWi9Aw",
+          },
         }
+      )
+      if (response.ok) {
+        // the comment has been sent succesfully!!
+        alert("Comment was sent!")
+      } else {
+        console.log("error")
+        alert("something went wrong")
+      }
+    } catch (error) {
+      console.log("error")
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.asin !== this.props.asin) {
-            this.setState({
-                comment: {
-                    ...this.state.comment,
-                    elementId: this.props.asin
-                }
-            })
-        }
-    }
-
-    sendComment = async (e) => {
-        e.preventDefault()
-        try {
-            let response = await fetch('https://striveschool-api.herokuapp.com/api/comments', {
-                method: 'POST',
-                body: JSON.stringify(this.state.comment),
-                headers: {
-                    'Content-type': 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw'
-                }
-            })
-            if (response.ok) {
-                // the comment has been sent succesfully!!
-                alert('Comment was sent!')
-            } else {
-                console.log('error')
-                alert('something went wrong')
+  return (
+    <div>
+      <Form onSubmit={sendComment}>
+        <Form.Group>
+          <Form.Label>Comment text</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Add comment here"
+            value={comment.comment}
+            onChange={(e) =>
+              setComment({
+                ...comment,
+                comment: e.target.value,
+              })
             }
-        } catch (error) {
-            console.log('error')
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Form onSubmit={this.sendComment}>
-                    <Form.Group>
-                        <Form.Label>Comment text</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Add comment here"
-                            value={this.state.comment.comment}
-                            onChange={e => this.setState({
-                                comment: {
-                                    ...this.state.comment,
-                                    comment: e.target.value
-                                }
-                            })}
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Rating</Form.Label>
-                        <Form.Control as="select" value={this.state.comment.rate}
-                            onChange={e => this.setState({
-                                comment: {
-                                    ...this.state.comment,
-                                    rate: e.target.value
-                                }
-                            })}>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        )
-    }
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Rating</Form.Label>
+          <Form.Control
+            as="select"
+            value={comment.rate}
+            onChange={(e) =>
+              setComment({
+                ...comment,
+                rate: e.target.value,
+              })
+            }
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
 }
 
 export default AddComment
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,53 +1,60 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react"
+import CommentList from "./CommentList"
+import AddComment from "./AddComment"
+import Loading from "./Loading"
+import Error from "./Error"
+import { useState } from "react"
+import { useEffect } from "react"
 
-class CommentArea extends Component {
+const CommentArea = ({ asin }) => {
+  const [comments, setComments] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
-    state = {
-        comments: [], // comments will go here
-        isLoading: false,
-        isError: false
-    }
+  useEffect(() => {
+    loadComments()
+  }, [asin])
 
-    componentDidUpdate = async (prevProps) => {
-        if (prevProps.asin !== this.props.asin) {
-            this.setState({
-                isLoading: true
-            })
-            try {
-                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
-                    headers: {
-                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw'
-                    }
-                })
-                console.log(response)
-                if (response.ok) {
-                    let comments = await response.json()
-                    this.setState({ comments: comments, isLoading: false, isError: false })
-                } else {
-                    console.log('error')
-                    this.setState({ isLoading: false, isError: true })
-                }
-            } catch (error) {
-                console.log(error)
-                this.setState({ isLoading: false, isError: true })
-            }
+  const loadComments = async () => {
+    setIsLoading(true)
+
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" + asin,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWRjM2ZlM2YyNjM3ODAwMTVlNTBkMjMiLCJpYXQiOjE2NDE5MDg0NTMsImV4cCI6MTY0MzExODA1M30.EdzYJUXtXimORoGl8bgWRQ3JSeQPmTemWyvDbJWi9Aw",
+          },
         }
-    }
+      )
+      console.log(response)
+      if (response.ok) {
+        let comments = await response.json()
 
-    render() {
-        return (
-            <div>
-                {this.state.isLoading && <Loading />}
-                {this.state.isError && <Error />}
-                <AddComment asin={this.props.asin} />
-                <CommentList commentsToShow={this.state.comments} />
-            </div>
-        )
+        setComments(comments)
+        setIsLoading(false)
+        setIsError(false)
+      } else {
+        console.log("error")
+        setIsLoading(false)
+        setIsError(true)
+      }
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+      setIsError(true)
     }
+  }
+
+  return (
+    <div>
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  )
 }
 
 export default CommentArea
